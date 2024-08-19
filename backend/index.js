0014e8b08cad175aba8505e5438ee4cca9dd30e9
Kontrain <panhaict@gmail.com>
2024-08-19 13:37:52 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,12 +17,13 @@ app.use(express.json({ limit: '10mb' }))
 app.use(cors())
 const dbname = "schoolcool";
 mongoose
-    .connect(mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0.1eexyf1.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0, {
+    .connect(`mongodb+srv://${process.env.atlasUser}:${process.env.atlasPassword}@cluster0.1eexyf1.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
-    .then(console.log("Connected to MongoDB"))
-    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err))
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
+
 
 app.use('/', Routes);
 
